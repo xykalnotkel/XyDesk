5a88f9397d6864e962e2901defaefe195c887093
Operator - XyDesk Team <operator.xydesk@users.noreply.github.com>
2026-09-15 01:47:45 +0000

    gunakan Android API stabil untuk build native
    Izin: SESI-20260914-OPERATOR-LANJUT

--- android-native/app/build.gradle.kts
+++ android-native/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.xystudio.xydesk.nativeclient"
-    compileSdk = 37
+    compileSdk = 35
     ndkVersion = "27.2.12479018"
     defaultConfig {
         applicationId = "com.xystudio.xydesk.native"
